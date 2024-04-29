36e9a9246f4ccdf778ec3f962229db6a8338595d
Owaisshahali <168421202+Owaisshahali@users.noreply.github.com>
2024-04-29 16:28:01 +0500

    Add files via upload

---
+++ squarecalculator.py
@@ -0,0 +1,10 @@
+# Ye script user se ek number input leta hai aur uska square print karta hai.
+
+# Number input lena
+num = float(input("1:"))
+
+# Square calculate karna
+square = num ** 2
+
+# Square print karna
+print("1:", square)
